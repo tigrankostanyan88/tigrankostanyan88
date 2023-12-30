4229d052e00056a5970540c28f74852dc46cd09e
Tigran <tigrankostanyan88@gmail.com>
2023-12-30 23:31:06 +0400

    update functional add test form

--- public/Admin/assets/js/main.js
+++ public/Admin/assets/js/main.js
@@ -5,6 +5,8 @@ textarea.forEach(el => {
 });
 
 
+
+
 let addQuestions = document.querySelector('.add_questions');
 let count = 1;
 
@@ -13,6 +15,7 @@ function createElement() {
 
     let questions = document.createElement('div');
     questions.classList.add('question');
+    questions.id = 11;
     questionList.appendChild(questions);
 
     let answerList = document.createElement('div');
@@ -44,7 +47,7 @@ function createElement() {
         btn.addEventListener('click', () => {
             let answerBox = document.createElement('div');
             answerBox.classList.add('answerBox');
-
+            answerBox.id = count++;
             let answer = document.createElement('textarea');
             let select = document.createElement('select');
             let option = document.createElement('option');
@@ -81,14 +84,12 @@ function createElement() {
     });
 }
 
-
-addQuestions.addEventListener('click', () => {
+addQuestions.addEventListener('click', (e) => {
     let questionCountEl = document.querySelector('.questionCount');
-
     questionCountEl.innerHTML = `Թեստը ունի ${count++} հարց`;
-    createElement();
-})
-
+    const parentElement = e.target.parentElement;
+    createElement();    
+});
 
 
 const addTestForm = document.querySelector('.add_test');
@@ -100,24 +101,56 @@ addTestForm.addEventListener("submit", (e) => {
 });
 
 function constructObject() {
+    let formdatad = new FormData(addTestForm);
     let testObject = {
         title: addTestForm.elements.title.value,
         questions: []
     };
 
-    for (let i = 0; i < addTestForm.elements.length; i++) {
-        let field = addTestForm.elements[i];
-        if ((field.name === 'question' || field.name === 'img')) {
+    let question = [];
+    let answers = [];
 
-            // question[field.name] = field.value;
-            testObject.questions.push({
-                text: 'name' + field.value,
-                curr: false
-            });
+    formdatad.forEach((value, key) => {
+        if(key === 'question') question.push(value);
+        if(key === 'img') question.push(value);
+        if(key === 'text') answers.push(value);
+        if(key === 'correct') answers.push(value);
+    });
+
+    for(let i = 0; i < question.length; i += 2) {
+        let questionObj = { answers: [] };
+
+        const questions = question[i+1];
+        const img = question[i];
+        questionObj.questions = questions;
+        questionObj.img = img;
+
+        for(let j = 0; j < answers.length; j += 2) {
+
+            const correct = answers[j + 1];
+            const text = answers[j];
+
+            if (text === questions) {
+                questionObj.answers.push({ text, correct });          
+            }
         }
+        testObject.questions.push(questionObj);
     }
-    
-    console.log(testObject);
+
     console.log(testObject.questions);
 }
 
+
+
+
+
+   // axios('/api/v1/test', {
+    //     method: 'POST',
+    //     data: testObject
+    // })
+    // .then((res) => {
+    //     console.log(res);
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // })
